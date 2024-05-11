0d0e6dd82372b104b65cb9c6caec4f5e9c4d65ff
russell-rozenbaum <rjrozenbaum@gmail.com>
2024-05-10 20:22:46 -0400

    completed data collection for batting data, starting lineups, and schedules

--- collectData.py
+++ collectData.py
@@ -5,12 +5,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+
 class Data :
+
     def __init__(self) :
         # Indexed by team; holds indices to players
         self.teamData = {}
         self.schedule = []
-    
 
 
     def private_makeSoupFrom(self, url) :
@@ -23,12 +24,31 @@ class Data :
 
         # We collect/scrape data from given url
         soup = BeautifulSoup(html, 'html.parser')
+
         driver.quit()
+
         return soup
     
+    
+    def private_map(self, team) :
+        match team :
+            case 'WSH':
+                return 'WSN'
+            case 'CWS' :
+                return 'CHW'
+            case 'SF' :
+                return 'SFG'
+            case 'TB' :
+                return 'TBR'
+            case 'SD' :
+                return 'SDP'
+            case 'KC' :
+                return 'KCR'
+            case _ :
+                return team
 
 
-    def collectPlayerData(self) :
+    def collectTeamData(self) :
 
         url = 'https://www.baseball-reference.com/leagues/majors/2024-standard-batting.shtml'
         soup = self.private_makeSoupFrom(url)
@@ -60,29 +80,11 @@ class Data :
             'hits' : int(player_hits[i].text.strip()),
             'hr' : int(player_hr[i].text.strip())
             }
-            if team not in self.teamData:
-                self.teamData[team] = {'batterData': {}, 'pitcherData': {}}
-            self.teamData[team]['batterData'][name] = player_info
-
-
-
-    def printBatterDataToCSV(self) :
-        c = ','
-        # We write scraped data to playerData.csv file
-        orig_out = sys.stdout
-        fout = open('playerData.csv', 'w')
-        sys.stdout = fout
-
-        print('playerName,team,gamesPlayed,plateApp,atBats,hits,homeRuns')
-        for team_name, team_data in self.teamData.items():
-            for player_name, player in team_data['batterData'].items():
-                print(player_name, c, team_name, c, player['gp'],
-                       c, player['pa'], c, player['pa'], c, player['hits'],
-                         c, player['hr'], sep='')
-        # Close altered stdout
-        sys.stdout = orig_out
-        fout.close()
-
+            if name != 'LgAvg per 600 PA' :
+                if team not in self.teamData :
+                    self.teamData[team] = {'batterData': {}, 'pitcherData': {}, 'pitcher': str, 'startingLineup': []}
+                self.teamData[team]['batterData'][name] = player_info
+        self.collectStartingLineups()
 
 
     def collectScheduleData(self) :
@@ -97,9 +99,55 @@ class Data :
         for game in matchups :
             teams = game.split(' @ ')
             self.schedule.append({'away': teams[0], 'home': teams[1]})
-
         
 
+
+    def collectStartingLineups(self) :
+        url = 'https://www.rotowire.com/baseball/daily-lineups.php'
+        soup = self.private_makeSoupFrom(url)   
+
+        # This line actually fails to grab a lineup if the game has started
+        boxes = soup.find_all('div', class_='lineup is-mlb')
+        for box in boxes :
+            away_team = box.find('div', class_='lineup__team is-visit')
+            away_team = away_team.find('div', class_='lineup__abbr').text.strip()         
+            home_team = box.find('div', class_='lineup__team is-home')  
+            home_team = home_team.find('div', class_='lineup__abbr').text.strip() 
+            # This just matches abbreviations where the 2 websites differ
+            away_team = self.private_map(away_team)
+            home_team = self.private_map(home_team)
+
+            away_lineup = box.find('ul', class_='lineup__list is-visit')
+            away_lineup = away_lineup.find_all('li', class_='lineup__player')
+            for chunk in away_lineup :
+                away_lineup_player = chunk.find('a').text.strip().replace('*','').replace('#','')
+                self.teamData[away_team]['startingLineup'].append(away_lineup_player)
+            home_lineup = box.find('ul', class_='lineup__list is-home')
+            home_lineup = home_lineup.find_all('li', class_='lineup__player')
+            for chunk in home_lineup :
+                home_lineup_player = chunk.find('a').text.strip().replace('*','').replace('#','')
+                self.teamData[home_team]['startingLineup'].append(home_lineup_player)
+            
+
+
+    def printTeamDataToCSV(self) :
+        c = ','
+        # We write scraped data to playerData.csv file
+        orig_out = sys.stdout
+        fout = open('teamData.csv', 'w')
+        sys.stdout = fout
+
+        print('playerName,team,gamesPlayed,plateApp,atBats,hits,homeRuns')
+        for team_name, team_data in self.teamData.items():
+            for player_name, player in team_data['batterData'].items():
+                print(player_name, c, team_name, c, player['gp'],
+                       c, player['pa'], c, player['pa'], c, player['hits'],
+                         c, player['hr'], sep='')
+        # Close altered stdout
+        sys.stdout = orig_out
+        fout.close()
+
+        
     def printScheduleToCSV(self) :
         c = ','
         # We write scraped data to playerData.csv file
@@ -111,6 +159,29 @@ class Data :
         for game in self.schedule :
             print(game['away'], c, game['home'], sep='')
 
+        # Close altered stdout
+        sys.stdout = orig_out
+        fout.close()
+
+
+    def printStartingLineupsToCSV(self) :
+        c = ','
+        # We write scraped data to playerData.csv file
+        orig_out = sys.stdout
+        fout = open('startingLineups.csv', 'w')
+        sys.stdout = fout
+        lineupSize = 9
+
+        print('team,1st,2nd,3rd,4th,5th,6th,7th,8th,9th')
+        for team_name, team in self.teamData.items():
+            print(team_name, c, sep='', end='')
+            if len(team['startingLineup']) < lineupSize :
+                for i in range(lineupSize) :
+                    print('', c, sep='', end='')
+            else :
+                for player in team['startingLineup'] :
+                    print(player, c, sep='', end='')
+            print('')
         # Close altered stdout
         sys.stdout = orig_out
         fout.close()
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import collectData
+import threading
 
 # Change, depending on whether we need to scrape and refresh data
-refreshData = False
+refreshData = True
 refreshSchedule = True
 
 data = collectData.Data()
 
-data.collectPlayerData() if refreshData else None
-data.collectScheduleData() if refreshSchedule else None
 
+# We simultaneously collect data from the web and create data structures
+player_data_thread = threading.Thread(target=data.collectTeamData)
+schedule_data_thread = threading.Thread(target=data.collectScheduleData)
+
+player_data_thread.start()
+schedule_data_thread.start()
+
+schedule_data_thread.join()
+player_data_thread.join()
+
+data.printTeamDataToCSV()
 data.printScheduleToCSV()
+data.printStartingLineupsToCSV()
+
+
+
 
 
 
